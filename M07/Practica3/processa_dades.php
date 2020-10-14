d5443c6b6e7ac6c95f60a44cf108828e4fdf1714
dsanchezndaw <dsanchezn@fp.insjoaquimmir.cat>
2020-10-14 17:39:18 +0200

    practica 3 formulari

--- M07/Practica3/processa_dades.php
+++ M07/Practica3/processa_dades.php
@@ -1,6 +1,14 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-   //Processar les dades
-} else {
-   //Pintar el formulari
+print_r("Texto: ".$_REQUEST["mytext"]);
+echo "<br>";
+print_r("Has seleccionado la radio: ".$_REQUEST["myradio"]);
+echo "<br>";
+if (isset($_REQUEST["mycheckbox"])){
+   print_r( $_REQUEST["mycheckbox"]);
+   echo "<br>";
 }
+print_r("Has seleccionado el item: ".$_REQUEST["myselect"]);
+echo "<br>";
+print_r("Texto: ".$_REQUEST["mytextarea"]);
+echo "<br>";
+?>
